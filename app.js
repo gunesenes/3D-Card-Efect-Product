ce67ba8ef4d3eb9f1c333947485ae96d120add93
Enes güneş <gunesenes.38@gmail.com>
2021-04-25 12:42:46 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,29 @@
+const card = document.querySelector(".card");
+const container = document.querySelector(".container");
+const title = document.querySelector(".title");
+const sneaker = document.querySelector(".sneaker img");
+const purchase = document.querySelector(".purchase");
+const description = document.querySelector(".info h3");
+const sizes = document.querySelector(".sizes");
+
+container.addEventListener("mousemove", (e) => {
+  let xAxis = (window.innerWidth / 2 - e.pageX) / 25;
+  let yAxis = (window.innerHeight / 2 - e.pageY) / 25;
+  card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
+});
+
+container.addEventListener("mouseenter", (e) => {
+  card.style.transition = "none";
+  sneaker.style.transform = "translateZ(100px) rotateZ(395deg)"; 
+  sneaker.style.transition="all 2s"
+});
+
+container.addEventListener("mouseleave", (e) => {
+  card.style.transition = "all 0.5s ease";
+  card.style.transform = `rotateY(0deg) rotateX(0deg)`;
+  title.style.transform = "translateZ(0px)";
+  sneaker.style.transform = "translateZ(0px) rotateZ(0deg)";
+  description.style.transform = "translateZ(0px)";
+  sizes.style.transform = "translateZ(0px)";
+  purchase.style.transform = "translateZ(0px)";
+});
